060012c25cff1832754ee95cf7128d8ec555814d
migfeer <53237542+migfeer@users.noreply.github.com>
2026-07-17 22:07:03 -0500

    Implement search on hashtable

--- firmware/rfid/rfid.ino
+++ firmware/rfid/rfid.ino
@@ -1,12 +1,21 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
 // Hash variables
 #define HASH_SIZE 8
 #define ARRAY_SIZE 4
 #include <stdio.h>  
 #include <stdlib.h>
 
+#define RST_PIN  9			// constante para referenciar pin de reset
+#define SS_PIN  10			// constante para referenciar pin de slave select
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);	// crea objeto mfrc522 enviando pines de slave select y reset
+byte LecturaUID[4]; 				// crea array para almacenar el UID leido
+
 //Protoypes:
 unsigned int hashing(byte key[ARRAY_SIZE]);
-bool search(byte UID[ARRAY_SIZE]);
+void search(byte lectura[ARRAY_SIZE]);
 
 //Users node structure
 typedef struct userInfo
@@ -17,7 +26,7 @@ typedef struct userInfo
   struct userInfo *next;
 } userInfo;
 
-userInfo users[5];
+userInfo users[4];
 userInfo *hashtable[HASH_SIZE];
 
 
@@ -27,32 +36,35 @@ bool acceso;
 void setup() 
 {
   Serial.begin(9600);
-  //byte key1[4]= {0xC1, 0x2F, 0xD6, 0x0E};
+  SPI.begin();				// inicializa bus SPI
+  mfrc522.PCD_Init();			// inicializa modulo lector
 
   //Register users:
   // TODO: Crear variables temporales para UID's
   byte key1[ARRAY_SIZE] = {0xC1, 0x2F, 0xD6, 0x2E};
   byte key2[ARRAY_SIZE] = {0xAF, 0x4A, 0xD2, 0xA1};
-  byte key3[ARRAY_SIZE] = {0x34, 0x6E, 0x6A, 0xB3};
-  byte key4[ARRAY_SIZE] = {0x45, 0x1F, 0x5D, 0x3A};
+  byte key3[ARRAY_SIZE] = {0x79, 0xC7, 0x00, 0x7F}; // correct user
+  byte key4[ARRAY_SIZE] = {0x79, 0xC7, 0x00, 0x6F};
 
   memcpy(users[0].UID, key1, 4);
   memcpy(users[1].UID, key2, 4);
   memcpy(users[2].UID, key3, 4);
   memcpy(users[3].UID, key4, 4);
 
-  strcpy(users[0].username, "Fer");
+  strcpy(users[0].username, "Pedro");
   strcpy(users[1].username, "Jesus");
   strcpy(users[2].username, "Karina");
-  strcpy(users[3].username, "Pedro");
+  strcpy(users[3].username, "Sr. Stark");
 
   int size = sizeof(users) / sizeof(users[0]);
-
   for (int i = 0; i < size; i++)
   {
     int hash;
     hash = hashing(users[i].UID);
-
+    //Serial.print("User: ");
+    //Serial.print(i+1);
+    //Serial.print("  | Hash: ");
+    //Serial.println(hash);
     //Create a node
     userInfo *u = (userInfo *)malloc(sizeof(userInfo)); //Type casting - malloc retorna un generic pointer
     if (u == NULL){
@@ -67,15 +79,45 @@ void setup()
     //TODO: Add prepeding and avoid memory leak
     if (hashtable[hash] == NULL){
         hashtable[hash] = u;
+        Serial.print("New user slot: "); 
+        Serial.println(hash); 
     } else {
         u->next = hashtable[hash];
         hashtable[hash] = u;
+        Serial.print("New user slot: "); 
+        Serial.println(hash); 
     }
   }
 }
 
 void loop() {
   
+  if ( ! mfrc522.PICC_IsNewCardPresent())		// si no hay una tarjeta presente
+    return;						// retorna al loop esperando por una tarjeta
+  
+  if ( ! mfrc522.PICC_ReadCardSerial()) 		// si no puede obtener datos de la tarjeta
+    return;						// retorna al loop esperando por otra tarjeta
+
+  Serial.print("UID:");				// muestra texto UID:
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {	
+    // bucle recorre de a un byte por vez el UID
+    if (mfrc522.uid.uidByte[i] < 0x10)
+    {		// si el byte leido es menor a 0x10
+      Serial.print(" 0");				// imprime espacio en blanco y numero cero
+    }
+    else{						// sino
+      Serial.print(" ");				// imprime un espacio en blanco
+    }
+    Serial.print(mfrc522.uid.uidByte[i], HEX);   	// imprime el byte del UID leido en hexadecimal
+    LecturaUID[i]=mfrc522.uid.uidByte[i];   	// almacena en array el byte del UID leido      
+  }
+
+  Serial.print("\t");   			// imprime un espacio de tabulacion 
+  search(LecturaUID);
+
+  mfrc522.PICC_HaltA();  		// detiene comunicacion con tarjeta
+  Serial.println("Swipe car");
   /*
     acceso = search(UID);
     if (acceso){
@@ -102,21 +144,29 @@ unsigned int hashing(byte key[ARRAY_SIZE])
 
 
 //Search function
-bool search(byte UID[ARRAY_SIZE])
+void search(byte lectura[ARRAY_SIZE])
 {
   // Hashing 
-  int hash = hashing(UID);
+  int hash = hashing(lectura);
   userInfo *ptr;
   ptr = hashtable[hash];
 
   // Search in hash table
   while (ptr != NULL)
   {
-    if ( (ptr->UID[0] == UID[0]) && (ptr->UID[1] == UID[1]) && (ptr->UID[2] == UID[2]) && (ptr->UID[4] == UID[4]) )
+    for (int i = 0; i < mfrc522.uid.size; i++)
     {
-      return true;
+      if(lectura[i] != ptr->UID[i])
+      {
+        break;
+      }
+      Serial.println("Acceso concedido");
+      Serial.print("Bienvenido: ");
+      Serial.println(ptr->username);
+      return;
     }
     ptr = ptr->next;
   }
-  return false;
+  Serial.println("Acceso denegado");
+  return;
 }
